d64c5595285aa036c458473527b958123a22c0da
SyRoxS <lmoreaumenanteau@gmail.com>
2023-06-23 10:14:46 +0200

    main

---
+++ WatchlistPropre.py
@@ -0,0 +1,53 @@
+import re 
+import csv
+
+DateOfInsertInList = []
+Rating = [] #8
+ReleaseDate = [] #13
+Genres = []
+Runtime = [] #9
+
+with open("WATCHLIST.csv", mode="r", encoding="utf8") as OpenFile:
+    FileContent = csv.reader(OpenFile)
+    next(FileContent)  # Supprime la première ligne car pas de films présents dessus
+    
+    for row in FileContent:
+        Genres.append(row[11])
+        DateOfInsertInList.append(row[3])
+        Rating.append(row[8])
+        ReleaseDate.append(row[13])
+        Runtime.append(row[9])
+
+def LongestWatch():
+    Total = 0 
+    Longest = 0
+    for EnumerationOfTime in Runtime:
+        Total = int(Total) + int(EnumerationOfTime)
+        if int(EnumerationOfTime) > Longest:
+            Longest = int(EnumerationOfTime) 
+    print("Durée total de visionage de films :",round((Total/60),1),"heures")
+    print("Durée du film le plus long :",Longest,"minutes")
+
+
+def StatGENDER():
+    GenderSeparated = []
+    AllDifferentGenders = []
+    #Action,Adventure,SciFi,Horror,Family,Drama,Romance    = 0
+    for EnumerationOfGenres in Genres:
+        GenderSeparated = GenderSeparated + EnumerationOfGenres.split(",")
+    AllDifferentGenders = set(GenderSeparated)
+
+    
+    
+    for Genders in AllDifferentGenders:
+        PercentGenders = {}
+        PercentGenders[Genders] = round(GenderSeparated.count(Genders)/len(GenderSeparated)*100,2)
+        print(PercentGenders)
+
+    
+
+        
+    
+
+StatGENDER()
+LongestWatch()
